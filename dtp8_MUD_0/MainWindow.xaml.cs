9b0cf12f382588810af9ad3b1e34329996f10a38
Anders Lundholm Fors <mr.fors@gmail.com>
2023-03-22 15:53:03 +0100

    få uppdateringar

--- dtp8_MUD_0/MainWindow.xaml.cs
+++ dtp8_MUD_0/MainWindow.xaml.cs
@@ -16,42 +16,6 @@ using System.Windows.Shapes;
 
 namespace dtp8_MUD_0
 {
-    class Room
-    {
-        // Constants:
-        public const int North = 0;
-        public const int East = 1;
-        public const int South = 2;
-        public const int West = 3;
-        public const int NoDoor = -1;
-
-        // Object attributes:
-        public int number;
-        public string roomname = "";
-        public string story = "";
-        public string imageFile = "";
-        public int[] adjacent = new int[4]; // adjacent[Room.North] etc.
-        public Room(int num, string name)
-        {
-            number = num; roomname = name;
-        }
-        public void SetStory(string theStory)
-        {
-            story = theStory;
-        }
-        public void SetImage(string theImage)
-        {
-            imageFile = theImage;
-        }
-        public void SetDirections(int N, int E, int S, int W)
-        {
-            adjacent[North] = N; adjacent[East] = E; adjacent[South] = S; adjacent[West] = W;
-        }
-        public int GetNorth() => adjacent[North];
-        public int GetEast() => adjacent[East];
-        public int GetSouth() => adjacent[South];
-        public int GetWest() => adjacent[West];
-    }
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
---
+++ dtp8_MUD_0/Player.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dtp8_MUD_0
+{
+    internal class Player
+    {
+    }
+}
